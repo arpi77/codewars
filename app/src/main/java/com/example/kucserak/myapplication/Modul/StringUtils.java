d10d85d50722697cf20fc85acae1fb3f04c0234b
Arpád Kucserák <kucserak@hq.eset.com>
2017-06-26 14:47:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/kucserak/myapplication/Modul/StringUtils.java
@@ -0,0 +1,12 @@
+package com.example.kucserak.myapplication.Modul;
+
+/**
+ * Created by kucserak on 26.6.2017.
+ */
+
+public class StringUtils {
+    public static String makeUpperCase(String str) {
+        if (str == null) return null;
+        return str.toUpperCase();
+    }
+}
---
+++ app/src/test/java/com/example/kucserak/myapplication/StringUtilsTests.java
@@ -0,0 +1,24 @@
+package com.example.kucserak.myapplication;
+
+import com.example.kucserak.myapplication.Modul.StringUtils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class StringUtilsTests {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals("HELLO", StringUtils.makeUpperCase("hello"));
+    }
+
+    @Test
+    public void addition_isCorrect2() throws Exception {
+        assertEquals("HELLO", StringUtils.makeUpperCase(null));
+    }
+}
